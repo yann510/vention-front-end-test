7b0e582d8bdc05fe21d53c21c1709d421d3e4212
Yann Thibodeau <y.thibodeau1@gmail.com>
2020-06-28 16:20:56 -0400

    Added all services to docker

--- src/api/src/index.ts
+++ src/api/src/index.ts
@@ -1,6 +1,6 @@
 import "./LoadEnv" // Must be the first import
 import logger from "./shared/Logger"
-import app from "./server"
+import app from "./Server"
 
 // Start the server
 const port = Number(process.env.PORT || 8080)
